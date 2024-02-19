2d1da59dd5173a0076d7cb93af3b7fa3f9ba91d2
Márcio Fontes <contato@marciofontes.com>
2024-02-19 10:02:47 -0300

    Enviando projeto 1

---
+++ Projeto1-Imprimir-Nome/projeto1-imprime-nome.rb
@@ -0,0 +1,11 @@
+puts "Digite o seu nome:"
+nome = gets.chomp
+
+puts "Digite o seu sobrenome:"
+sobrenome = gets.chomp
+
+puts "Digite a sua idade:"
+idade = gets.chomp
+
+puts "\nBem-vindo a nossa biblioteca:"
+puts "Seu nome é #{nome} #{sobrenome} e você tem #{idade} anos."
